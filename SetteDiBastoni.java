b4fbfd28be6e91aae1c945cc013f930d0a08fa0c
Francesco Debbi <131989006+FrancescoDettoDebbi@users.noreply.github.com>
2023-05-21 21:09:12 +0200

    Update SetteDiBastoni.java

--- SetteDiBastoni.java
+++ SetteDiBastoni.java
@@ -8,7 +8,25 @@ public class SetteDiBastoni extends CartaVanilla{
 	}
 	
 	public void inGioco() {
-		
+		System.out.print("La briscola è BASTONI!");
 	}
 
+	public boolean isWinning(Carta altraCarta, Carta.Seme briscola) {
+		if (getSeme().equals(Carta.Seme.BASTONI) && !(altraCarta.getSeme().equals(Carta.Seme.BASTONI))) {
+			return true;
+		} else if (altraCarta.getSeme().equals(Carta.Seme.BASTONI) && !(getSeme().equals(Carta.Seme.BASTONI))) {
+			return false;
+		} else {
+			if (getValore().getIndiceNumerico() > altraCarta.getValore().getIndiceNumerico()) {
+				return true;
+			} else {
+				return false;
+			}
+		}
+	}
+	
+	@Override
+	public String toString() {
+		return super.toString() + "@Se questa carta viene giocata per prima, la briscola è BASTONI per questa mano.";
+	}
 }
